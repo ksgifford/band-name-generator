0acd01bf6eda55a9e1638e4df88a68b85d38a5aa
Kevin Gifford <ksgifford@gmail.com>
2015-09-30 20:48:09 -0700

    first commit of band name generator

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/app/"));
+
+var Adjective = function() {
+  this.fun = true;
+  this.tall = true;
+  this.dark = true;
+  this.handsome = true;
+};
+
+var adjective = new Adjective();
+
+function getRandomWord(object) {
+  var propArray = Object.keys(object);
+  var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
+  return {word: randomProp};
+}
+
+app.listen(port, function() {
+  console.log('server started on port ' + port);
+});
+
+app.get('/adjective', function(req, res) {
+  res.json(getRandomWord(adjective));
+});
+
+app.get("/", function(req, res) {
+  res.send("hello, universe!");
+});
