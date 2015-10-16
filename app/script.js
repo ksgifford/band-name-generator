c17c9ad627de09c2b0fb81f01098afa6cc88c5d5
Kevin Gifford <ksgifford@gmail.com>
2015-10-15 23:06:40 -0700

    Corrected submittal

--- app/script.js
+++ app/script.js
@@ -3,17 +3,17 @@
 $(function() {
 
   $('button').click(function() {
-    $.get('http://localhost:3000/adjective', function(response) {
+    $.get('adjective', function(response) {
       var adjective = response.word;
       $('#adjective').text(adjective);
     });
 
-    $.get('http://localhost:3000/verb', function(response) {
+    $.get('verb', function(response) {
       var verb = response.word;
       $('#verb').text(verb);
     });
 
-    $.get('http://localhost:3000/noun', function(response) {
+    $.get('noun', function(response) {
       var noun = response.word;
       $('#noun').text(noun);
     });
